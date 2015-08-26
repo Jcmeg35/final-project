00863984b44759aef4a4cd293615eaad7bf92315
Jacob <Jacobemeier@gmail.com>
2015-08-26 17:31:56 -0400

    editing to styling of rfp show and create pages.

--- app/controllers/rfps_controller.rb
+++ app/controllers/rfps_controller.rb
@@ -1,18 +1,10 @@
 class RfpsController < ApplicationController
-  # autocomplete :rfp, :order_size
+
    
 
-  # def index
-  #   if params[:order_size]
-  #     @rfp = Rfp.where(order_size: params[:order_size]).paginate(page: params[:page], per_page: 30)
-  #   elsif params[:id]
-  #     @rfp = Rfp.where(id: params[:id]).paginate(page: params[:page], per_page: 30)
-  #   elsif params[:ship_size]
-  #     @rfp = Rfp.product.where(ship_size: params[:rfp_ship_size]).paginate(page: params[:page], per_page: 30)
-  #   else
-  #     @rfp = Rfp.paginate(page: params[:page], per_page: 30)
-  #   end
-  # end
+  def index
+    @rfps = Rfp.order(end_date: :desc).paginate(page: params[:page], per_page: 20)
+  end
 
   def new
     @rfp = Rfp.new
